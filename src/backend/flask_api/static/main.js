515505e122f3936d17a1870128739e66ddaefb60
Suraj Kothari <surajvkothari@gmail.com>
2022-02-02 16:44:22 +0000

    Initialised basic Vue application

---
+++ src/backend/flask_api/static/main.js
@@ -0,0 +1,12 @@
+// import Vue from 'vue';
+//import App from '@/App.vue';
+//import router from '@/router';
+
+// new Vue({
+//   router,
+//   render: (h) => h(App),
+// }).$mount('#konla-app');
+var testObject = new Vue({
+  el: '#test',
+  data: {message: 'Hello World!'}
+})
