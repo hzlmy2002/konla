1ff9dbfa589db687ba6cdf074b991f26070ecc91
Bartosz Grabek <bartoszgrabek7@gmail.com>
2022-03-27 01:39:51 +0100

    optimized section patterns, added last section cutoff with ref section

--- src/backend/apiServer/konla/PaperProcessor/sections.py
+++ src/backend/apiServer/konla/PaperProcessor/sections.py
@@ -2,16 +2,9 @@ import spacy
 
 # eg. '1 Introduction \n', note: will not match lowercase stopwords
 heading_pattern_strict = [
-    {"ORTH": "\n", "OP": "+"}, #
+    {"ORTH": "\n", "OP": "+"},
     {"IS_DIGIT": True},
-    {"IS_TITLE": True, "OP":"+"},
-    {"ORTH": "\n", "OP": "+"} # BEFORE: {"IS_SENT_START": False, "OP":"+"}
-]
-
-heading_pattern_strict_dotted = [
-    {"ORTH": "\n", "OP": "+"}, # 
-    {"IS_DIGIT": True},
-    {"ORTH": "."},# {"IS_PUNCT": True},
+    {"ORTH": ".", "OP": "?"},
     {"IS_TITLE": True, "OP":"+"},
     {"ORTH": "\n", "OP": "+"}
 ]
@@ -22,53 +15,36 @@ checking letter case after matching """
 heading_pattern_loose = [
     {"ORTH": "\n", "OP": "+"}, # 
     {"IS_DIGIT": True},
+    {"ORTH": ".", "OP": "?"},
     {"IS_ALPHA": True, "OP":"+"},
     {"ORTH": "\n", "OP": "+"}
 ]
 
-heading_pattern_loose_dotted = [
-    {"ORTH": "\n", "OP": "+"}, # 
-    {"IS_DIGIT": True},
-    {"ORTH": "."}, # {"IS_PUNCT": True},
-    {"IS_ALPHA": True, "OP":"+"},
-    {"ORTH": "\n", "OP": "+"}
-]
-
-# eg. '4.2 Summary'
-subheading_pattern_1 = [ 
-    {"ORTH": "\n", "OP": "+"}, # 
-    {"IS_DIGIT": True, "TEXT": {"REGEX": "[0-9]+\.[0-9]+"}},
-    {"IS_TITLE": True, "OP": "+"},
-    {"IS_SENT_START": True, "OP":"!"}
-]
-subheading_pattern_2 = [
-    {"ORTH": "\n", "OP": "+"}, # 
-    {"TEXT": {"REGEX": "[0-9]+\.[0-9]+"}},
-    {"IS_TITLE": True, "OP": "+"},
-    {"IS_SENT_START": True, "OP":"!"}
-]
-subheading_pattern_dotted = [ #
-    {"ORTH": "\n", "OP": "+"},
-    {"TEXT": {"REGEX": "[0-9]+\.[0-9]+"}},
-    {"ORTH": "."}, # {"IS_PUNCT": True},
-    {"IS_TITLE": True, "OP": "+"},
-    {"IS_SENT_START": True, "OP":"!"}
-]
+# eg. '4.2 Summary'# eg. '4.2 Summary'
 # works similarly to heading_pattern_loose
 subheading_pattern_loose = [
     {"ORTH": "\n", "OP": "+"}, # 
     {"TEXT": {"REGEX": "[0-9]+\.[0-9]+"}},
-    {"IS_ALPHA":True, "OP": "+"},
+    {"ORTH": ".", "OP": "?"},
+    {"IS_ALPHA": True, "OP": "+"},
     {"ORTH": "\n", "OP": "+"}
 ]
-subheading_pattern_loose_dotted = [
+subheading_pattern_loose_ascii = [
     {"ORTH": "\n", "OP": "+"}, # 
     {"TEXT": {"REGEX": "[0-9]+\.[0-9]+"}},
-    {"ORTH": "."}, # {"IS_PUNCT": True},
-    {"IS_ALPHA":True, "OP": "+"},
+    {"ORTH": ".", "OP": "?"},
+    {"IS_ASCII": True, "OP": "+"},
     {"ORTH": "\n", "OP": "+"}
 ]
 
+# Reference Section Pattern Matching
+ref_section = [
+    {"ORTH": "\n", "OP": "+"},
+    {"TEXT": {"REGEX": "[0-9]"}, "OP": "?"},
+    {"ORTH": ".", "OP":"?"},
+    {"LOWER": {"IN": ["references", "bibliography"]}},
+    {"ORTH": "\n", "OP": "+"}
+]
 
 class SectionExtractor():
     """
@@ -76,24 +52,23 @@ class SectionExtractor():
     If text segmentation follows standard enumaration, 1., 1.1., 1.2, 1.3, etc.,
     it discerns ranges for sections and subsections.
     First section title that does not follow the numbering will include the last
-    token of the previous section
+    token of the previous section. Last section is cutoff at first token of 
+    reference section if found, otherwise the last token of text.
     """
     def __init__(self, doc, nlp):
-        self.doc = doc
-        self.nlp = nlp # model
+        self.doc = doc  # doc must be the result of applying spacy lang model to raw text
+        self.nlp = nlp  # model
         self.segments = []
+        self.refs = []  # holds titles for reference sections
+        self.refs_start_tk = len(doc)  # start tk of reference section, default end of doc
 
     def _get_matches(self):
         matcher = spacy.matcher.Matcher(self.nlp.vocab)
         matcher.add("heading_strict", [heading_pattern_strict])
-        matcher.add("heading_strict_dotted", [heading_pattern_strict_dotted])
         matcher.add("heading_loose", [heading_pattern_loose])
-        matcher.add("heading_loose_dotted", [heading_pattern_loose_dotted])
-        matcher.add("subheading_1", [subheading_pattern_1])
-        matcher.add("subheading_2", [subheading_pattern_2])
-        matcher.add("subheading_dotted", [subheading_pattern_dotted])
         matcher.add("subheading_loose", [subheading_pattern_loose])
-        matcher.add("subheading_loose_dotted", [subheading_pattern_loose_dotted])
+        matcher.add("subheading_loose_ascii", [subheading_pattern_loose_ascii])
+        matcher.add("reference_section", [ref_section])
         return matcher(self.doc)
 
     def _check_matches(self, matches):
@@ -101,22 +76,31 @@ class SectionExtractor():
         Returns a list of all valid matches in a 4-tuple form:
         (match_text, match_string_id, match_start_tk, match_end_token)
         eg. ("1. Introduction \n", "heading_strict", 52, 55)
+        Excludes sections labeled matched by 'reference_section' pattern
         """
         matched = []
         for match_id, start, end in matches:
             string_id = self.nlp.vocab.strings[match_id]
             span = self.doc[start:end]
             match_text = span.text
-            #print(match_text)
-            # check if all non-stopwords are capitalized (use if loose patterns)
-            valid = True
-            for tk in span:
-                if not(tk.like_num or tk.is_stop or tk.is_punct or tk.is_digit or tk.is_space):
-                    valid = tk.is_title
-                    if valid == False:
-                        break
-            if valid:
-                matched.append((match_text, string_id, start, end))
+            if string_id == "reference_section":
+                self.refs.append(match_text)
+                self.refs_start_tk = min([start, self.refs_start_tk])
+            else:  # check if non-reference for recognized as sections
+                if not(match_text in self.refs):
+                    # check if all non-stopwords are capitalized or upper (use if loose patterns)
+                    valid = True
+                    for tk in span:
+                        if not(tk.like_num or tk.is_stop or tk.is_punct or tk.is_digit or tk.is_space):
+                            valid = tk.is_title or tk.is_upper
+                            if valid == False:
+                                break
+                    # check if no \n chars in match
+                    for tk in span[1:-2]:
+                        if tk.text == "\n":
+                            valid = False
+                    if valid:
+                        matched.append((match_text, string_id, start, end))
         return matched
     
     def _remove_copies_and_overlap(self, matched):
@@ -163,9 +147,9 @@ class SectionExtractor():
                 end = len(self.doc)  # take last token number
             # Append (section_name, start_tk, end_tk, length_in_tokens)
             ranges.append((section_now[0], start, end, end-start))
-        # Append last section
+        # Append last section, cutoff at refs_start_tk
         last_sec = final[-1]
-        ranges.append((last_sec[0], last_sec[3], len(self.doc), len(self.doc)-last_sec[3]))
+        ranges.append((last_sec[0], last_sec[3], self.refs_start_tk, self.refs_start_tk-last_sec[3]))
         return ranges
 
     def run(self):
